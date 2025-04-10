0e8b875b609c65cbea613fdea5b83fe687f89233
st-binhvo <binh.vo@supremetech.vn>
2025-04-10 09:03:29 +0700

    feat: return data from sdk when pop activity

--- internalsdk/src/main/java/com/example/internalsdk/InternalSDKActivity.kt
+++ internalsdk/src/main/java/com/example/internalsdk/InternalSDKActivity.kt
@@ -1,23 +1,29 @@
 package com.example.internalsdk
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.internalsdk.ui.theme.KotlinKnowledgeTheme
 
 class InternalSDKActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,6 +32,12 @@ class InternalSDKActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android123",
+                        onPop = {
+                            val resultIntent = Intent()
+                            resultIntent.putExtra("result_key", "This is the result string")
+                            setResult(RESULT_OK, resultIntent)
+                            finish() // This pops the activity
+                        },
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -35,7 +47,9 @@ class InternalSDKActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String,onPop : () -> Unit, modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("Hello") }
+
     Column {
         Text(
             text = "Hello $name!",
@@ -44,13 +58,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 .fillMaxSize(),
             color = Color.Red
         )
-    }
-}
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Label") }
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinKnowledgeTheme {
-        Greeting("Android")
+        Button(onClick = {
+           onPop()
+        }) {
+            Text("Filled")
+        }
+
     }
 }
